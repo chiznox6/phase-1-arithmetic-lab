75f5b46d322d39a740fec9ba3bc9951b939131f7
James <james.kariuki5@student.moringaschool.com>
2025-06-15 11:48:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Define the variables as expected by the tests
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; // 62
+
+let random = Math.floor(Math.random() * 100) + 1; // random > 0
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // 4
+
+let max = Math.max(1, 2, 3, 10, 20); // 20
 
 // Write your code here
